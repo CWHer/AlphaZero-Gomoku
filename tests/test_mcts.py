892271b00c98aa6989e2b500cea5b6450a2eeed8
cwher <cwher@outlook.com>
2022-03-28 17:17:34 +0800

    test: upd mcts test

--- tests/test_mcts.py
+++ tests/test_mcts.py
@@ -1,13 +1,11 @@
-import torch
 from icecream import ic
 
 from agent.mcts import MCTSPlayer
 from agent.network import PolicyValueNet
 from env.simulator import Simulator
 
+
 env = Simulator()
-env.print()
-# ic(env.getEmptyIndex())
 env.step(Simulator.Coord2Idx((1, 1)))
 env.step(Simulator.Coord2Idx((2, 2)))
 env.step(Simulator.Coord2Idx((1, 2)))
@@ -16,11 +14,12 @@ env.step(Simulator.Coord2Idx((1, 3)))
 env.step(Simulator.Coord2Idx((4, 4)))
 env.step(Simulator.Coord2Idx((1, 4)))
 env.step(Simulator.Coord2Idx((5, 5)))
-env.print()
-# ic(env.isDone())
-# env.backtrack(1)
+ic(len(env.getEmptyIndices()))
+env.display()
+ic(env.isEnd())
+# env.backtrack()
 net = PolicyValueNet()
-net.setDevice(torch.device("cpu"))
+net.setDevice("cpu")
 player = MCTSPlayer(net)
 action, mcts_prob = player.getAction(env, is_train=True)
 ic(env.Idx2Coord(action))

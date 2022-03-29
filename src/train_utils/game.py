407ee5b4e6510a1d04fe723a3ffc81351a157891
cwher <cwher@outlook.com>
2022-03-29 20:44:04 +0800

    feat: upd game

--- src/train_utils/game.py
+++ src/train_utils/game.py
@@ -1,98 +1,89 @@
+from typing import List, Tuple
+
 import numpy as np
 import torch
 from agent.mcts import MCTSPlayer
+from agent.network import PolicyValueNet
 from env.simulator import Simulator
-from icecream import ic
+from utils import printInfo
 
 
-def selfPlay(net, seed):
+def selfPlay(net: PolicyValueNet, seed: int) -> \
+        Tuple[List[np.ndarray], List[np.ndarray], np.ndarray]:
     """[summary]
-    self play and gather experiences
-    Args:
-        net ([type]): [description]
     Returns:
-        states [List[np.ndarray]]: [description]
-        mcts_probs [List[np.ndarray]]: [description]
-        values [List[float]]: [description]
+        (states, mcts_probs, mcts_vals)
     """
-    # initialize seeds (torch, np, random)
+    # fix seeds (torch, np, random)
     torch.manual_seed(seed)
     np.random.seed(seed)
 
     env = Simulator()
+    done, winner = env.isEnd()
 
-    data_buffer, episode_len = [], 0
+    episode_len, data_buffer = 0, []
     players = [MCTSPlayer(net) for _ in range(2)]
 
-    while True:
-        episode_len += 1
-
-        # NOTE: getAction MUST NOT change env
-        # NOTE: data = (features(states), mcts_probs)
-        action, data = players[env.turn].getAction(env, is_train=True)
+    while not done:
+        # NOTE: data = (states, mcts_probs)
+        action, data = \
+            players[env.turn].getAction(env, is_train=True)
         data_buffer.append(data)
 
-        # take action
         env.step(action)
-        # update root
+        # env.display()
+        episode_len += 1
+        # update MCTS root node
         for i in range(2):
-            players[i].updateRoot(action)
-
-        # debug
-        # env.print()
+            players[i].step(action)
 
         # check game status
-        done, winner = env.isDone()
+        done, winner = env.isEnd()
 
-        if done:
-            # FIXME: NOTE: drop data of draws
-            if winner == -1:
-                ic("Game Over. Draw")
-                return [], [], []
-            message = "Game Over. Player{} win!".format(winner)
-            # ic(message)
+    states, mcts_probs = zip(*data_buffer)
 
-            states, mcts_probs = zip(*data_buffer)
-            values = [
-                1 if (i & 1) == winner else -1
-                for i in range(episode_len)]
-            # ic(len(states), len(values))
+    if winner == -1:
+        printInfo("Game Over. Draw")
+        return states, mcts_probs, \
+            np.zeros(episode_len, dtype=np.float32)
 
-            return states, mcts_probs, values
+    # printInfo(f"Game Over. Player {winner} win!")
+    mcts_vals = np.array(
+        [1 if (i & 1) == winner else -1
+         for i in range(episode_len)], dtype=np.float32
+    )
+    return states, mcts_probs, mcts_vals
 
 
-def contest(net0, net1, seed):
+def contest(
+    net0: PolicyValueNet, net1: PolicyValueNet,
+        seed: int) -> int:
     """[summary]
     contest between net0 and net1
 
     Returns:
-        int: [description]. winner
+        winner
     """
-    # initialize seeds (torch, np, random)
+    # fix seeds (torch, np, random)
     torch.manual_seed(seed)
     np.random.seed(seed)
 
     env = Simulator()
+    done, winner = env.isEnd()
     players = [MCTSPlayer(net0), MCTSPlayer(net1)]
 
-    while True:
-        # NOTE: getAction MUST NOT change env
-        action, _ = players[env.turn].getAction(env)
+    while not done:
+        action, _ = \
+            players[env.turn].getAction(env)
 
-        # take action
         env.step(action)
-        # update root
+        # env.display()
+        # update MCTS root node
         for i in range(2):
-            players[i].updateRoot(action)
-
-        # debug
-        # env.print()
+            players[i].step(action)
 
         # check game status
-        done, winner = env.isDone()
-        if done:
-            message = "Game Over. {}".format(
-                "Draw" if winner == -1
-                else "Player{} win!".format(winner))
-            # ic(message)
-            return winner
+        done, winner = env.isEnd()
+
+    # printInfo(f"Game Over. Winner: {winner}")
+    return winner

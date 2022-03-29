ddb83f6e98bd491aad8462511e91561093fcfec1
cwher <cwher@outlook.com>
2022-03-29 21:18:09 +0800

    fix: fix conv bug

--- src/agent/network_utils.py
+++ src/agent/network_utils.py
@@ -46,8 +46,11 @@ class ObsEncoder():
 
 
 def conv3x3(in_channels, out_channels):
+    # NOTE: BN subtracts mean, which cancels the effect of bias
     return nn.Conv2d(
-        in_channels, out_channels, kernel_size=3, padding=1)
+        in_channels, out_channels,
+        kernel_size=3, padding=1, bias=False
+    )
 
 
 class ResBlock(nn.Module):

ef6aadb7cd69b1c5d881183dbc90c751f05b44e9
cwher <cwher@outlook.com>
2022-03-29 20:44:57 +0800

    feat: upd training pipeline

--- src/train.py
+++ src/train.py
@@ -1,125 +1,142 @@
 import copy
-import multiprocessing
-from multiprocessing import Pool
+import random
+from functools import partial
 
-import numpy as np
-import torch
-from icecream import ic
+from tensorboardX import SummaryWriter
+from torch import multiprocessing
+from torch.multiprocessing import Pool
 from tqdm import tqdm
 
 from agent.network import PolicyValueNet
-from config import TRAIN_CONFIG
+from config import DATA_CONFIG, TRAIN_CONFIG
 from train_utils.game import contest, selfPlay
 from train_utils.replay_buffer import ReplayBuffer
-from utils import timeLog
+from utils import printInfo, printWarn, timeLog
 
 
 class Trainer():
-    def __init__(self) -> None:
+    def __init__(self, loc="cpu") -> None:
         self.net = PolicyValueNet()
-        self.cnt = 0  # version of best net
+        self.net.setDevice(loc)
+        self.buffer = ReplayBuffer()
+
+        self.n_best = 0
         self.best_net = copy.deepcopy(self.net)
-        self.replay_buffer = ReplayBuffer()
+
+        self.seed = partial(
+            random.randint, a=0, b=20000905)
+        self.writer = SummaryWriter(TRAIN_CONFIG.log_dir)
 
     @timeLog
-    def collectData(self):
-        ic("collect data")
-        # self.net.setDevice(torch.device("cpu"))
+    def __collectData(self):
+        printInfo("collect data")
+
+        n_game = TRAIN_CONFIG.n_game
 
         games = [
-            (self.net, np.random.randint(2 ** 30))
-            for _ in range(TRAIN_CONFIG.game_num)]
-        with tqdm(total=TRAIN_CONFIG.game_num) as pbar:
-            with Pool(TRAIN_CONFIG.process_num) as pool:
+            (self.net, self.seed())
+            for _ in range(n_game)
+        ]
+        with tqdm(total=n_game) as pbar:
+            with Pool(TRAIN_CONFIG.n_process) as pool:
                 results = pool.starmap(selfPlay, games)
                 for result in results:
-                    self.replay_buffer.add(*result)
+                    self.buffer.add(*result)
                     pbar.update()
 
     @timeLog
-    def evaluate(self):
-        ic("evaluate model")
-        # self.net.setDevice(torch.device("cpu"))
-        # self.best_net.setDevice(torch.device("cpu"))
+    def __evaluate(self):
+        printInfo("evaluate model")
 
-        results = {0: 0, 1: 0, -1: 0}
-        with tqdm(total=TRAIN_CONFIG.num_contest) as pbar:
+        logs = {0: 0, 1: 0, -1: 0}
+        n_contest = TRAIN_CONFIG.n_contest
+        with tqdm(total=n_contest) as pbar:
             games = [
-                (self.net, self.best_net, np.random.randint(2 ** 30))
-                for _ in range(TRAIN_CONFIG.num_contest // 2)]
-            with Pool(TRAIN_CONFIG.process_num) as pool:
-                winners = pool.starmap(contest, games)
-                for winner in winners:
-                    results[winner] += 1
+                (self.net, self.best_net, self.seed())
+                for _ in range(n_contest // 2)
+            ]
+            with Pool(TRAIN_CONFIG.n_process) as pool:
+                results = pool.starmap(contest, games)
+                for winner in results:
+                    logs[winner] += 1
                     pbar.update()
 
             games = [
-                (self.best_net, self.net, np.random.randint(2 ** 30))
-                for _ in range(TRAIN_CONFIG.num_contest // 2)]
-            with Pool(TRAIN_CONFIG.process_num) as pool:
-                winners = pool.starmap(contest, games)
-                for winner in winners:
-                    winner = winner ^ 1 if winner != -1 else winner
-                    results[winner] += 1
+                (self.best_net, self.net, self.seed())
+                for _ in range(n_contest // 2)
+            ]
+            with Pool(TRAIN_CONFIG.n_process) as pool:
+                results = pool.starmap(contest, games)
+                for winner in results:
+                    if winner != -1:
+                        winner ^= 1
+                    logs[winner] += 1
                     pbar.update()
 
-        message = "result: {} win, {} lose, {} draw".format(
-            results[0], results[1], results[-1])
-        ic(message)
-        return (results[0] + 0.5 * results[-1]) / TRAIN_CONFIG.num_contest
+        printInfo(
+            f"win: {logs[0]}, lose: {logs[1]}, draw: {logs[-1]}")
 
-    def train(self):
-        ic("train model")
-        # self.net.setDevice(torch.device("cuda:0"))
+        return (logs[0] + 0.5 * logs[-1]) / n_contest
 
-        train_iter = self.replay_buffer.trainIter()
-        for i in range(1, TRAIN_CONFIG.train_epochs + 1):
-            losses, mean_loss, mean_acc = [], 0, 0
-            with tqdm(total=len(train_iter)) as pbar:
-                for data_batch in train_iter:
-                    loss, acc = self.net.trainStep(data_batch)
-                    losses.append(loss)
-                    mean_loss += loss * data_batch[-1].shape[0]
-                    mean_acc += acc * data_batch[-1].shape[0]
-                    pbar.update()
-            print("epoch {} finish".format(i))
-            mean_loss /= self.replay_buffer.size()
-            mean_acc /= self.replay_buffer.size()
-            ic(mean_loss, mean_acc)
+    def __train(self, epoch):
+        printInfo("train model")
+
+        train_iter = self.buffer.sample()
+        n_batch, mean_loss, mean_acc = 0, 0, 0
+        with tqdm(total=len(train_iter)) as pbar:
+            for data_batch in train_iter:
+                n_batch += 1
+                loss, acc = \
+                    self.net.trainStep(data_batch)
+                mean_loss += loss
+                mean_acc += acc
+                pbar.update()
+
+        mean_loss /= n_batch
+        mean_acc /= n_batch
+        self.writer.add_scalar("loss", mean_loss, epoch)
+        self.writer.add_scalar("accuracy", mean_acc, epoch)
+        printInfo(
+            "loss: {:>.4f}, accuracy: {:>.4f}".format(
+                mean_loss, mean_acc)
+        )
 
     def run(self):
         """[summary]
-        pipeline: collect data, train, evaluate, update and repeat
+        NOTE: training pipeline contains:
+             1. collect data    2. train model
+             3. evaluate model  4. update best model
         """
-        for i in range(1, TRAIN_CONFIG.train_num + 1):
+
+        for i in range(TRAIN_CONFIG.epochs):
+            printInfo(f"=====Epoch {i}=====")
+
             # >>>>> collect data
-            self.collectData()
-            print("Round {} finish, buffer size {}".format(
-                i, self.replay_buffer.size()))
+            self.__collectData()
+            printInfo(f"buffer size {len(self.buffer)}")
             # save data
-            if i % TRAIN_CONFIG.data_save_freq == 0:
-                self.replay_buffer.save(version=str(i))
+            if (i + 1) % DATA_CONFIG.save_freq == 0:
+                self.buffer.save(version=f"epoch{i}")
 
-            # >>>>> train
-            if self.replay_buffer.enough():
-                self.train()
+            # >>>>> train model
+            if self.buffer.enough():
+                self.__train(epoch=i)
 
-            # >>>>> evaluate
-            if i % TRAIN_CONFIG.check_freq == 0:
-                win_rate = self.evaluate()
-                if win_rate >= TRAIN_CONFIG.update_threshold:
-                    self.cnt += 1
+            # >>>>> evaluate model
+            if (i + 1) % TRAIN_CONFIG.eval_freq == 0:
+                if self.__evaluate() >= \
+                        TRAIN_CONFIG.update_thr:
+                    # update best model
+                    self.n_best += 1
                     self.best_net = copy.deepcopy(self.net)
-                    self.best_net.save(version=str(self.cnt))
-                    message = "new best model {}!".format(self.cnt)
-                    ic(message)
+                    self.best_net.save(version=self.n_best)
+                    printInfo(f"best model {self.n_best}")
                 else:
-                    ic("reject new model.")
+                    printWarn(True, "reject new model")
 
 
 if __name__ == "__main__":
-    # NOTE: multiprocessing with CUDA is available on Linux
     multiprocessing.set_start_method("spawn")
 
-    trainer = Trainer()
+    trainer = Trainer(loc="cuda:0")
     trainer.run()

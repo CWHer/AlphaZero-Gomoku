ae756bd5cf51104a92f636a8160b50d0e55e9b86
CWHer <cwher@outlook.com>
2022-04-10 03:15:33 +0000

    test: upd MCTS test

--- tests/test_mcts.py
+++ tests/test_mcts.py
@@ -1,18 +1,39 @@
-from multiprocessing import Process
-
 from icecream import ic
 
-from agent.batch_inference import SharedData, batchInference
-from agent.net_mcts import MCTSPlayer
-from agent.network import PolicyValueNet
 from env.simulator import Simulator
 
 
-def func(env, index, shared_data):
-    player = MCTSPlayer(index, shared_data)
-    action, mcts_prob = player.getAction(env, is_train=True)
+def testNetMCTS(env):
+    from multiprocessing import Process
+
+    from agent.batch_inference import SharedData, batchInference
+    from agent.net_mcts import MCTSPlayer
+    from agent.network import PolicyValueNet
+
+    def func(env, index, shared_data):
+        player = MCTSPlayer(index, shared_data)
+        action, mcts_prob = player.getAction(env, is_train=True)
+        ic(env.Idx2Coord(action))
+        shared_data.finish()
+
+    net = PolicyValueNet()
+    net.setDevice()
+
+    with SharedData(n_proc=1) as shared_data:
+        shared_data.reset()
+        proc = Process(target=func, args=(env, 0, shared_data))
+        proc.start()
+
+        batchInference(shared_data, net)
+        proc.join()
+
+
+def testMCTS(env):
+    from agent.mcts import MCTSPlayer
+
+    player = MCTSPlayer(n_search=4000)
+    action, _ = player.getAction(env)
     ic(env.Idx2Coord(action))
-    shared_data.finish()
 
 
 env = Simulator()
@@ -29,15 +50,7 @@ env.display()
 ic(env.isEnd())
 # env.backtrack()
 
-net = PolicyValueNet()
-net.setDevice()
-
-with SharedData(n_proc=1) as shared_data:
-    shared_data.reset()
-    proc = Process(target=func, args=(env, 0, shared_data))
-    proc.start()
-
-    batchInference(shared_data, net)
-    proc.join()
+testMCTS(env)
+# testNetMCTS(env)
 
 print("Done")
